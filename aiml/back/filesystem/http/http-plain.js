0459f45bc17671a27b1211a74be0bbc2140214b1
AA <asif.22b1531176@abes.ac.in>
2025-02-12 12:17:25 +0530

    updated file

--- aiml/back/filesystem/http/http-plain.js
+++ aiml/back/filesystem/http/http-plain.js
@@ -1,16 +1,18 @@
+const { promises } = require('dns');
 const http = require('http');
-const server = http.createServer((req, res) =>
+const fs = require('fs/promises')
+const server = http.createServer(async(req, res) =>
 {
   console.log(req.url)
   res.statuscode=200;
-  res.setHeader('content-type', 'text/plain');
-  res.write("hello, world!");
-  res.end();
+  res.setHeader('content-type', 'text/html');
+  res.write("<h1>hello, world!</h1>");
+  const data = await fs.readFile("./home.html","utf8");
+  res.end(data);
 
 });
-//port = 3000;
+port = 3000;
 
-server.listen(() =>{
-    const a = server.address();
-    console.log(`server is running on port ${a.port}`);
+server.listen(port,() =>{
+    console.log(`server is running on port ${port}`);
 });
