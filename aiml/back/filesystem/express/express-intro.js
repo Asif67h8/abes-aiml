35b3c8c2c05f72bb8334f257dbdaf9cc6c828469
AA <asif.22b1531176@abes.ac.in>
2025-02-21 10:23:14 +0530

    updated files

---
+++ aiml/back/filesystem/express/express-intro.js
@@ -0,0 +1,35 @@
+//const express=require('express');
+//http://localhost:3000/api?name=rajesh&rollno=220233
+import express from 'express';
+const app= express();
+const port=3000;
+app.get('/', (req, res) =>{
+    res.send("welcome to express")
+});
+app.get('/api/:name/:rollno',(req, res)=>{
+    try {
+        const {name,rollno} = req.params;
+        res.send(`welcome ${name} and your roll no. is ${rollno}`);
+        
+    } catch (error) {
+        console.log(`error is: ${error.massage}`)
+        
+    }
+});
+/*app.get('/api', (req, res) =>{
+   // const {name="guest",rollno= 100} = req.query;
+    try {
+        const {name,rollno}= req.query;
+    if(!name){
+        return res.status(400).send("name is required");
+    }
+    else{
+        res.send(`welcome to ABES: ${name} and roll no. id ${rollno}`)
+    }
+    } catch (error) {
+        console.log(error.message)
+    }
+});*/
+app.listen(port,() => {
+    console.log(`server is running on port ${port}`);
+});
